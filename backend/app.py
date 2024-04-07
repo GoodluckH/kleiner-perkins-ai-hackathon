0e0fa79b92ad10e1d6f2ef41bdc88b0699c36ef7
nataliya-a <nkarmarkar@dons.usfca.edu>
2024-04-07 16:05:15 -0700

    add route to GET embeddings for a word

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,5 @@
-from flask import Flask
-
+from flask import Flask, jsonify, request
+from model import get_word_embedding
 
 app = Flask(__name__)
 
@@ -9,3 +9,13 @@ app = Flask(__name__)
 )
 def home():
     return "Hello, World!"
+
+
+@app.route(
+    "/word", methods=["GET"]
+)
+def process_word():
+    word = request.args.get('word', '')
+    embedding = get_word_embedding(word)
+    return jsonify({"word": word, "embedding": embedding})
+
